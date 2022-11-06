20bdd3a7caa3f131e160c3f02b8340449a720167
johnxiler <johnxiler@gmail.com>
2022-11-06 21:42:04 +0800

    update css and html codes

--- adminloginPage.php
+++ adminloginPage.php
@@ -6,31 +6,26 @@
       rel="stylesheet"
     />
 </head>
-<div class="container">
+<div class="content">
         <div class="row">
             <div class="login-box">
-                <div class="login-key">
-                    <i class="fa fa-key" aria-hidden="true"></i>
+            <div class="login-key">
+              <img id="loginlogo" src="/static/images/logo.png" alt="brgyviilogo"/>
                 </div>
-                <div class="login-title">
-                    ADMIN PANEL
+                 <div class="login-title">     
+                       <p>LOGIN ACCOUNT</p>
                 </div>
                     <div class="login-form">
                         <form action="includes/login-inc.php" method="post">
                             <div class="form-group">
-                                <label class="form-control-label">USERNAME</label>
+                                <label class="form-control-label">USERNAME: </label>
                                 <input name= "username" type="text" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label class="form-control-label">PASSWORD</label>
+                                <label class="form-control-label">PASSWORD: </label>
                                 <input name="password" type="password" class="form-control" i>
                             </div>
-
-                            <div class="col-lg-12 loginbttm">
-                                <div class="col-lg-6 login-btm login-text">
-                                    <!-- Error Message -->
-                                </div>
-                                <div class="col-lg-6 login-btm login-button">
+                                <div class="login-button">
                                     <button name="login" type="submit" class="btn btn-outline-primary">LOGIN</button>
                                 </div>
                             </div>
